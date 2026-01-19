7d02efe77a65fe4ef4054cf3fb575e4cb1ff9e12
EURO-12068\Profesor <ralbiol>
2026-01-19 13:33:21 +0100

    Código 19/01/2026

---
+++ iniciandoenjs/ejercicio1/codigo.js
@@ -0,0 +1,30 @@
+function sumar(){
+   let num1 = parseInt(document.getElementById("num1").value);
+   let num2 = parseInt(document.getElementById("num2").value);
+   let suma = num1 + num2;
+   document.getElementById("resultado").innerText = suma; 
+}
+
+function restar(){
+   let num1 = parseInt(document.getElementById("num1").value);
+   let num2 = parseInt(document.getElementById("num2").value);
+   let resta = num1 - num2;
+   document.getElementById("resultado").innerText = resta; 
+}
+
+function multiplicar(){
+   let num1 = parseInt(document.getElementById("num1").value);
+   let num2 = parseInt(document.getElementById("num2").value);
+   let multi = num1 * num2;
+   document.getElementById("resultado").innerText = multi; 
+}
+
+function dividir(){
+   let num1 = parseInt(document.getElementById("num1").value);
+   let num2 = parseInt(document.getElementById("num2").value);
+   let divi = num1 / num2;
+   document.getElementById("resultado").innerText = divi; 
+}
+
+
+
